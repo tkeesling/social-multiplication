010da163bc16dc4700d4cc2c1ece8df7156a46e7
tyler.keesling <tyler.keesling@import.io>
2019-04-11 13:35:56 -0600

    Add the first controller for multiplication and unit tests

---
+++ src/main/java/com/multiplication/social/ws/MultiplicationController.java
@@ -0,0 +1,28 @@
+package com.multiplication.social.ws;
+
+import com.multiplication.social.domain.Multiplication;
+import com.multiplication.social.service.MultiplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The class implements a REST API for the Multiplication app.
+ */
+@RestController
+@RequestMapping("/multiplications")
+public class MultiplicationController {
+
+    @Autowired
+    private final MultiplicationService multiplicationService;
+
+    public MultiplicationController(MultiplicationService multiplicationService) {
+        this.multiplicationService = multiplicationService;
+    }
+
+    @GetMapping("/random")
+    Multiplication getRandomMultiplication() {
+        return multiplicationService.createRandomMultiplication();
+    }
+}
