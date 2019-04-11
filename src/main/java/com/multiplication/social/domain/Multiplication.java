89ae8826383ede83ca98d403894a5ac524da3b2a
tyler.keesling <tyler.keesling@import.io>
2019-04-11 10:15:28 -0600

    Add project lombok and refactor Multiplication domain

--- src/main/java/com/multiplication/social/domain/Multiplication.java
+++ src/main/java/com/multiplication/social/domain/Multiplication.java
@@ -1,37 +1,19 @@
 package com.multiplication.social.domain;
 
-public class Multiplication {
-  private int factorA;
-  private int factorB;
-  private int result;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
-  public Multiplication(int factorA, int factorB) {
-    this.factorA = factorA;
-    this.factorB = factorB;
-    this.result = factorA * factorB;
-  }
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class Multiplication {
+    private final int factorA;
+    private final int factorB;
 
-  public int getFactorA() {
-    return factorA;
-  }
-
-  public int getFactorB() {
-    return factorB;
-  }
-
-  public int getResult() {
-    return result;
-  }
-
-  @Override
-  public String toString() {
-    return "Multiplication{"
-        + "factorA="
-        + factorA
-        + ", factorB="
-        + factorB
-        + ", result(A*B)"
-        + result
-        + '}';
-  }
+    Multiplication() {
+        this(0, 0);
+    }
 }
