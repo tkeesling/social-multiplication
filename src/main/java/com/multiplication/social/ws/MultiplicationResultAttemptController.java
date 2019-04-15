625ce0cf806421e209404efe4b023d01004ee358
tyler.keesling <tyler.keesling@import.io>
2019-04-14 21:00:29 -0600

    Add stats to static page; added controller endpoint to get user stats

--- src/main/java/com/multiplication/social/ws/MultiplicationResultAttemptController.java
+++ src/main/java/com/multiplication/social/ws/MultiplicationResultAttemptController.java
@@ -4,10 +4,14 @@ import com.multiplication.social.domain.MultiplicationResultAttempt;
 import com.multiplication.social.service.MultiplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/results")
@@ -32,5 +36,10 @@ final class MultiplicationResultAttemptController {
         );
 
         return ResponseEntity.ok(attemptCopy);
+    }
+
+    @GetMapping
+    ResponseEntity<List<MultiplicationResultAttempt>> getStatistics(@RequestParam("alias") String alias) {
+        return ResponseEntity.ok(multiplicationService.getStatsForUser(alias));
     }
 }
